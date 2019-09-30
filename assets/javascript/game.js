4a5d92bc8aeb5a9b0dd1c8a74b10a7d3cd182d68
Dafne Paz <A01650434@itesm.mx>
2019-09-29 20:19:23 -0500

    fourth commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,14 @@
+
+
+
+var wins = 0;
+var losses = 0;
+var numberToGuess = "";
+var counter = 0;
+
+
+
+
 $(document).ready(function() {
 
     // Notice I didn't set $(".jumbotron") to a var this time?
@@ -5,11 +16,19 @@ $(document).ready(function() {
     $("#goal-btn").on("click", function() {
 
       // ... we generate a random number
-      var random = Math.floor(Math.random() * 100) + 19;
+    var random = Math.floor(Math.random() * 100) + 19;
 
       // ... and then dump the goal number into our goal-number div.
-      $("#goal-number").text(random);
+     $("#goal-number").text(random);
 
     });
 
-  });
+     });
+
+$(document).ready(function() {
+    var list = $('li').hide();
+     var i = 0;
+     (function showImg() {
+         list.eq(i++).fadeIn(400, showImg);
+            })();
+    });
